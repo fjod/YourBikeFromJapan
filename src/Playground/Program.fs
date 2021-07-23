2cd5fb7eda486f8e497d0ff510275bf42bb818b8
MichaelSnytko <fourjustoneday@yandex.ru>
2021-07-23 12:26:15 +0300

    dapper hello world works!

--- src/Playground/Program.fs
+++ src/Playground/Program.fs
@@ -1,12 +1,14 @@
 ﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
-open System.IO
-open System.IO.Compression
+open MySql.Data
+open System.Data
 open System.Net
 open System.Net.Http
 open System.Text.Json
 open System.Text.Json.Serialization
+open Dapper
+open MySql.Data.MySqlClient;
 
 [<CLIMutable>]
 type Id = {
@@ -47,7 +49,14 @@ type Bike = {
           starting_bid : int;
           starting_bid_rub : int;
           state : Object;
-          year :Object;
+          year :int;
+}
+[<CLIMutable>]
+type BikeModel = {
+    id : int
+    Maker : string
+    Model : string
+    Year : int
 }
 
 [<CLIMutable>]
@@ -57,12 +66,19 @@ type Root = {bikes : Bike seq  }
 let from whom =
     sprintf "from %s" whom
 
+let GetConnection()  =
+    new MySqlConnection("Server=localhost;Database=Bikes;Uid=root;Pwd=312312;") //:> IDbConnection
+
 
 
 [<EntryPoint>]
 let main argv =
-    let request = "https://projapan.ru/bikes?mileage_min=0&mileage_max=100&year_min=2009&year_max=2021&rank_min=0&rank_max=5&auction%5B%5D=auc&auction%5B%5D=bds&auction%5B%5D=jba&auction%5B%5D=arai&date=all&volume=50&manufacturer=H&model="
+    let request = "https://projapan.ru/bikes?mileage_min=0&mileage_max=100&year_min=2009&year_max=2021&rank_min=0&rank_max=5&auction%5B%5D=auc&auction%5B%5D=bds&auction%5B%5D=jba&auction%5B%5D=arai&date=all&volume=750&manufacturer=H&model="
     let client = new HttpClient()
+    let ins (data:BikeModel) (connection:IDbConnection ) =
+        connection.Query<BikeModel>($"insert into BikeModel (Maker, Model, Year) values ('{data.Maker}','{data.Model}',{data.Year});") |> ignore
+        ()
+
     async {
         let message = new HttpRequestMessage(HttpMethod.Get, request)
         message.Headers.Add ( "Host", "projapan.ru" )
@@ -74,7 +90,16 @@ let main argv =
         response.EnsureSuccessStatusCode () |> ignore
         let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
         let json = JsonSerializer.Deserialize<Root> content
-        Seq.iter (fun b -> printfn $"%s{b._id.ToString()}" ) json.bikes
+        let conv = Seq.map (fun b ->  {Maker = "Honda"
+                                       Model = b.model
+                                       Year = b.year
+                                       id = 0}) json.bikes
+
+
+        use conn = new MySqlConnection("Server=localhost;Port=6603;Database=Bikes;Uid=root;Pwd=312312;")
+        conn.Open ()
+        Seq.iter (fun b -> ins  b conn) conv
+        conn.Close ()
         ()
     } |>  Async.RunSynchronously
 
